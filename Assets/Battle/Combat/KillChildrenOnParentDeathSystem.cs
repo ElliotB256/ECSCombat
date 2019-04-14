56ca1a52796bc5ad84519afc014fd377476dbaf9
Elliot Bentine <ebentine@bitbucket.org>
2019-04-14 22:00:41 +0100

    Big ships can now also explode. Also added a system to cleanup broken target references when things die, which prevents some tasks from throwing errors.

---
+++ Assets/Battle/Combat/KillChildrenOnParentDeathSystem.cs
@@ -0,0 +1,54 @@
+﻿using Unity.Burst;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Transforms;
+
+namespace Battle.Combat
+{
+    /// <summary>
+    /// Destroys any entity whose Parent transform has died
+    /// </summary>
+    [UpdateAfter(typeof(DealAttackDamageSystem)), UpdateBefore(typeof(KillEntitiesWithNoHealthSystem))]
+    public class KillChildrenOnParentDeathSystem : JobComponentSystem
+    {
+        /// <summary>
+        /// God this sounds horrible
+        /// </summary>
+        [BurstCompile]
+        struct KillChildrenWithDeadParents : IJobForEachWithEntity<Parent>
+        {
+            [ReadOnly] public ComponentDataFromEntity<Health> health;
+            public EntityCommandBuffer.Concurrent buffer;
+
+            public void Execute(
+                Entity e,
+                int index,
+                [ReadOnly] ref Parent parent
+                )
+            {
+                if (!health.Exists(parent.Value))
+                    return;
+
+                if (health[parent.Value].Value < 0f)
+                    buffer.DestroyEntity(index, e);
+            }
+        }
+
+        private DeleteDeadEntitiesBuffer m_entityBufferSystem;
+
+        protected override void OnCreateManager()
+        {
+            m_entityBufferSystem = World.GetOrCreateSystem<DeleteDeadEntitiesBuffer>();
+        }
+
+        protected override JobHandle OnUpdate(JobHandle inputDependencies)
+        {
+            var health = GetComponentDataFromEntity<Health>(true);
+            var job = new KillChildrenWithDeadParents() { buffer = m_entityBufferSystem.CreateCommandBuffer().ToConcurrent(), health = health };
+            var jobHandle = job.Schedule(this, inputDependencies);
+            m_entityBufferSystem.AddJobHandleForProducer(jobHandle);
+            return jobHandle;
+        }
+    }
+}
---
+++ Assets/Battle/Combat/RemoveDeadTargetReferencesSystem.cs
@@ -0,0 +1,34 @@
+﻿using Unity.Burst;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+
+namespace Battle.Combat
+{
+    /// <summary>
+    /// Sets any Target components that point to an invalid, non-existant entity to Entity.Null
+    /// </summary>
+    [UpdateAfter(typeof(KillChildrenOnParentDeathSystem)), UpdateBefore(typeof(KillEntitiesWithNoHealthSystem))]
+    public class RemoveDeadTargetReferencesSystem : JobComponentSystem
+    {
+        [BurstCompile]
+        struct RemoveDeadTargetReferencesJob : IJobForEach<Target>
+        {
+            [ReadOnly] public ComponentDataFromEntity<Targetable> targetables;
+
+            public void Execute(ref Target target)
+            {
+                //If target cannot be targetted, detarget it.
+                if (targetables.Exists(target.Value))
+                    return;
+                target.Value = Entity.Null;
+            }
+        }
+
+        protected override JobHandle OnUpdate(JobHandle inputDependencies)
+        {
+            var targets = GetComponentDataFromEntity<Targetable>(true);
+            return new RemoveDeadTargetReferencesJob() { targetables = targets }.Schedule(this, inputDependencies);
+        }
+    }
+}
