0bd96d518c51c834fadb662f8bf8f9c5aab396e4
Elliot Bentine <ebentine@bitbucket.org>
2019-04-15 23:46:20 +0100

    Added some components for giving targeting orders to certain types of troop. System not yet implemented.

---
+++ Assets/Battle/AgentAI/Aggro/AgentCategory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Unity.Entities;
+
+namespace Battle.AI
+{
+    /// <summary>
+    /// Descriptive category of an entity 
+    /// </summary>
+    [Serializable]
+    public struct AgentCategory : IComponentData
+    {
+        [Flags] public enum eType : byte
+        {
+            Small,
+            Medium,
+            Large
+        }
+
+        public eType Type;
+    }
+}
---
+++ Assets/Battle/AgentAI/Aggro/TargetingOrders.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Unity.Entities;
+
+namespace Battle.AI
+{
+    /// <summary>
+    /// A set of hints as to how an entity should choose targets.
+    /// </summary>
+    [Serializable]
+    public struct TargetingOrders : IComponentData
+    {
+        /// <summary>
+        /// The categories that we prefer to engage.
+        /// </summary>
+        public AgentCategory Preferred;
+
+        /// <summary>
+        /// The categories that we do not want to engage.
+        /// </summary>
+        public AgentCategory Discouraged;
+    }
+}
